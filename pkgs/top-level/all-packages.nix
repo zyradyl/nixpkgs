ace9bc2be9a4c7408ed76dda14ad5d7dfcb46ed6
Franz Pletz <fpletz@fnordicwalking.de>
2017-07-31 23:59:56 +0200

    shellinabox: build with openssl 1.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5934,7 +5934,9 @@ in
 
   shout = nodePackages.shout;
 
-  shellinabox = callPackage ../servers/shellinabox { };
+  shellinabox = callPackage ../servers/shellinabox {
+    openssl = openssl_1_0_2;
+  };
 
   shrikhand = callPackage ../data/fonts/shrikhand { };
 
